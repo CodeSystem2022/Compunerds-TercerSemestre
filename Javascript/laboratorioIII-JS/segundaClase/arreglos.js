86e9b625a1b7f0e6075295441e8204e296d328cc
carlafaes <carlafaes18@hotmail.com>
2023-05-07 23:25:44 -0300

    c2

--- Javascript/laboratorioIII-JS/segundaClase/arreglos.js
+++ Javascript/laboratorioIII-JS/segundaClase/arreglos.js
@@ -1,2 +1,14 @@
 //let aytos = new Array("Audi","Ferrari","Twingo") Sintaxis obsoleta
-let autos= ["Audi","Ferrari","Twingo"];
+let autos = ["Audi", "Ferrari", "Twingo"];
+console.log(autos)
+
+//Recorrer un array de a un resultado por vez
+console.log(autos[0]);
+console.log(autos[1]);
+console.log(autos[2]);
+
+//Recorrer el array y devolcer todos los elementos
+//con el ciclo for
+for(let i=0; i<autos.length;i++){
+    console.log(autos[i]);
+}
