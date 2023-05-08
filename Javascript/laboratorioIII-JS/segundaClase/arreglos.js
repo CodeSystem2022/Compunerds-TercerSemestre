7837250f70b6754c5a8f89d0f7b5dce38d4894d3
carlafaes <carlafaes18@hotmail.com>
2023-05-07 23:34:54 -0300

    push

--- Javascript/laboratorioIII-JS/segundaClase/arreglos.js
+++ Javascript/laboratorioIII-JS/segundaClase/arreglos.js
@@ -10,5 +10,13 @@ console.log(autos[2]);
 //Recorrer el array y devolcer todos los elementos
 //con el ciclo for
 for(let i=0; i<autos.length;i++){
-    console.log(autos[i]);
+    console.log(i +":"+autos[i]);
 }
+
+//modificamos los elementos del arreglo
+autos[1]="Volvo";
+console.log(autos[1])
+
+//agregamos elementos al array
+autos.push("Jeep")
+console.log(autos)
