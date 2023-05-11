2b9e4b22604fbdc58f85072703a2f3d2f4047c7d
carlafaes <carlafaes18@hotmail.com>
2023-05-11 16:26:37 -0300

    parte 4

--- Javascript/laboratorioIII-JS/cuartaClase/01-04-objetos.js
+++ Javascript/laboratorioIII-JS/cuartaClase/01-04-objetos.js
@@ -15,3 +15,17 @@ let persona={
 }
 console.log("Su nombre es: "+persona.nombre,"y su apellido:"+persona.apellido)//accedemos a la porcion en memoria donde se guardan estas propiedades
 console.log(persona.nombreCompleto())
+
+let persona2= new Object();//Debe crear un nuevo objeto en memoria
+persona2.nombre="Juan";
+persona2.direccion="Salada 14";
+persona2.telefono=2920341575
+console.log(persona2)
+
+console.log(persona["apellido"])//Accedemos como si fuera un arreglo
+
+//for in
+for(propiedad in persona){
+    console.log(propiedad);
+    console.log(persona[propiedad])
+}
