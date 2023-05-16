4cba67a0809eb9a4abf172d0aa0f34019f36abf9
carlafaes <carlafaes18@hotmail.com>
2023-05-15 22:18:19 -0300

    metodos get y set

--- Javascript/laboratorioIII-JS/sextaClase/clases.js
+++ Javascript/laboratorioIII-JS/sextaClase/clases.js
@@ -6,7 +6,21 @@ class Persona{
     get nombre(){
         return this._nombre;
     }
+    set nombre(nombre){
+        this._nombre = nombre;
+    }
+    get apellido(){
+        return this._apellido;
+    }
+    set apellido(apellido){
+        this._apellido = apellido;
+    }
 }
 
 let persona1= new Persona("Martin", "Perez");
 console.log(persona1.nombre);
+console.log(persona1.apellido);
+persona1.nombre="Juan";
+persona1.apellido="Mar"
+console.log(persona1.nombre);
+console.log(persona1.apellido);
