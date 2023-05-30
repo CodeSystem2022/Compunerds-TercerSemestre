ed84800d864a77f06ecfc068e829155e7e2610b3
jsegovia <jsegovia@boxcustodia.com>
2023-05-30 13:28:34 -0300

    subo archivos para carga de codigo de la semana 7

---
+++ Javascript/laboratorioIII-JS/Clase 7 - Clases Parte 2.js
@@ -0,0 +1,14 @@
+/*
+Clase 7: Clases Parte 2
+Laboratorio III: JavaScript
+Team: CompuNerds
+Scrum Master: Nicolas Segovia
+*/
+
+// 7.1 Heredar métodos - Alumno:
+
+
+// 7.2 Sobreescritura - Alumno:
+
+
+// 7.3 Clase Object, toString, sobreescritura y Polimorfismo - Alumno:
