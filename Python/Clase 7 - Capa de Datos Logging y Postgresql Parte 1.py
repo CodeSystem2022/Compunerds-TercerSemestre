c655d1b055832a62f016585278bf1c32ccca2383
Kevin Sosa <kevinema90@gmail.com>
2023-05-30 23:37:33 -0300

    Update ejercicios SM7, kevin sosa

--- Python/Clase 7 - Capa de Datos Logging y Postgresql Parte 1.py
+++ Python/Clase 7 - Capa de Datos Logging y Postgresql Parte 1.py
@@ -1,3 +1,7 @@
+import logging as log
+
+
+
 """
 Clase 7: Capa de Datos 'Logging y Postgresql Parte 1'
 Laboratorio III: PYTHON
@@ -8,9 +12,17 @@ Scrum Master: Nicolas Segovia
 # 7.1 Explicación con el diagrama de clase UML (Tarea: hacer el diagrama UML) - Alumno:
 
 
-# 7.2 Manejo de logging: Parte 1 - Alumno:
+# 7.2 Manejo de logging: Parte 1 - Alumno: Kevin Sosa
 
-
+#Llamamos a la configuracion basica.
+if __name__ == '__main__':
+    log.basicConfig(level=log.INFO)
+    log.debug('Mensaje a nivel debug')
+    log.info('Mensaje a nivel info')
+    log.warning('Mensaje a nivel warning')
+    log.error('Mensaje a nivel error')
+    log.warning('Mensaje a nivel critical')
+    
 # 7.3 Manejo de logging: Parte 2 - Alumno:
 
 
