168b13f444bd1f8bb2c810591b3f334ba2aa7885
Giuliana Dealbera Etchechoury <giulydealbera@gmail.com>
2023-05-31 05:20:10 -0300

    Giu sube asistencia de js semana 7

--- Javascript/laboratorioIII-JS/Clase 7 - Clases Parte 2.js
+++ Javascript/laboratorioIII-JS/Clase 7 - Clases Parte 2.js
@@ -7,8 +7,15 @@ Scrum Master: Nicolas Segovia
 
 // 7.1 Heredar métodos - Alumno:
 
+class Empleado extends Persona{
 
-// 7.2 Sobreescritura - Alumno:
 
+
+    // 7.2 Sobreescritura - Alumno: Giuliana Dealbera Etchechoury
+    nombreCompleto(){
+        return super.nombreCompleto() + ', ' + this._departamento;
+    }
+}
+console.log(empleado1.nombreCompleto());
 
 // 7.3 Clase Object, toString, sobreescritura y Polimorfismo - Alumno:
