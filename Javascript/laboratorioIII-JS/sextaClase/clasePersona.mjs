4f1772e1aef289a45344e5670bf5d59e37998d97
carlafaes <carlafaes18@hotmail.com>
2023-06-04 22:17:03 -0300

    modificacion de extension de archivo

--- Javascript/laboratorioIII-JS/sextaClase/clasePersona.mjs
+++ Javascript/laboratorioIII-JS/sextaClase/clasePersona.mjs
@@ -1,17 +1,19 @@
+//Se realizo la modificacion de las extensiones de los archivos ClasePadre y ClaseEmpleado (de js a mjs), para que fueran tomados como modulos y poder realizar importaciones y exportaciones de clases. Y asi poder modularizar ambas clases
+
 class Persona {
   //clase padre
 
   //8.2 Atributos estáticos
   //static contadorObjetosPersona = 0;
   //8.4 Uso de la palabra static:
-  static contadorPersonas=0;
+  static contadorPersonas = 0;
   //8.3 Atributos estáticos vs No estáticos
   //email="Valor default email";
 
   constructor(nombre, apellido) {
     this._nombre = nombre;
     this._apellido = apellido;
-    this.idPersona= ++Persona.contadorPersonas;
+    this.idPersona = ++Persona.contadorPersonas;
     //Persona.contadorObjetosPersona++;
     //console.log("Se incrementa el contador; "+ Persona.contadorObjetosPersona);
   }
@@ -29,7 +31,7 @@ class Persona {
   }
   // 7.1 Heredar métodos - Alumno: Kevin Sosa
   nombreCompleto() {
-    return this.idPersona+" "+this._nombre + " " + this._apellido;
+    return this.idPersona + " " + this._nombre + " " + this._apellido;
   }
 
   //7.3 Clase Object, toString,sobreescritura y Polimorfismo
@@ -52,25 +54,6 @@ class Persona {
   }
 }
 
-class Empleado extends Persona {
-  //clase hija
-  constructor(nombre, apellido, departamento) {
-    super(nombre, apellido); //heredamos los campos de la clase padre
-    this._departamento = departamento;
-  }
-
-  get departamento() {
-    return this._departamento;
-  }
-
-  set departamento(departamento) {
-    this._departamento = departamento;
-  }
-  // 7.2 Sobreescritura - Alumno: Giuliana Dealbera Etchechoury
-  nombreCompleto() {
-    return super.nombreCompleto() + ", " + this._departamento;
-  }
-}
 
 let persona1 = new Persona("Martin", "Perez");
 console.log(persona1.nombre);
@@ -80,12 +63,7 @@ persona1.apellido = "Mar";
 console.log(persona1.nombre);
 console.log(persona1.apellido);
 
-let empleado1 = new Empleado("Marcelo", "Gimenez", "Sistemas");
-console.log(empleado1);
-console.log(empleado1.nombreCompleto());
-
 //Object.prototype.toString esta es la manera de acceder a atributos y metodos de manera dinamica
-console.log(empleado1.toString()); //se trabaja con el metodo de la clase hija.
 console.log(persona1.toString()); //se trabaja con el metodo de la clase padre.
 
 //llamamos metodo saludar
@@ -93,17 +71,15 @@ console.log(persona1.toString()); //se trabaja con el metodo de la clase padre.
 Persona.saludar(); // se ejecuta correctamente
 Persona.saludar2(persona1);
 
-Empleado.saludar();
-Empleado.saludar2(empleado1);
 
 //console.log(persona1.contadorObjetosPersona); undefined
 //console.log(Persona.contadorObjetosPersona);
 //console.log(Empleado.contadorObjetosPersona);
 
 //accedemos a los valores no estaticos
-console.log(persona1.email)
-console.log(empleado1.email);
-//console.log(Persona.email)  no se puede acceder desde la clase porque no es estatic 
+console.log(persona1.email);
+//console.log(Persona.email)  no se puede acceder desde la clase porque no es estatic
 
 console.log(persona1.toString());
-console.log(empleado1.toString());
+
+export default Persona;
