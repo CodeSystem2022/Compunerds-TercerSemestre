cb43889046018be485d243ac3e2684a8c01c3051
jsegovia <jsegovia@boxcustodia.com>
2023-06-06 23:30:05 -0300

    subo clase 8 de py. Dejo la clase 7 dentro de la carpeta con las demas clases, para ir modificando la 8 si asi se requiere

---
+++ Python/Clase 8/capa_datos_persona/persona_dao.py
@@ -0,0 +1,53 @@
+from Persona import Persona
+from conexion import Conexion
+from logger_base import log
+
+
+class PersonaDAO:
+    """
+    DAO significa: Data Access Object
+    CRUD significa:
+                    Create -> Insertar
+                    Read   -> Seleccionar
+                    Update -> Actualizar
+                    Delete -> Eliminar
+
+    """
+    _SELECCIONAR = 'SELECT * FROM persona ORDER BY id_persona'
+    _INSERTAR = 'INSERT INTO persona (nombre, apellido, email) VALUES (%s,%s,%s)'
+    _ACTUALIZAR = 'UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
+    _ELIMINAR = 'DELETE FROM persona WHERE id_persona=%s'
+
+    # Definimos los metodos de clase
+    @classmethod
+    def seleccionar(cls):
+        with Conexion.obtenerConexion():
+            with Conexion.obtenerCursor() as cursor:
+                cursor.execute(cls._SELECCIONAR)
+                registros = cursor.fetchall()
+                personas = []  # creamos una lista
+                for registro in registros:
+                    persona = Persona(registro[0], registro[1], registro[2], registro[3])
+                    personas.append(persona)
+                return personas
+
+    @classmethod
+    def eliminar(cls,persona):
+        with Conexion.obtenerConexion():
+            with Conexion.obtenerCursor() as cursor:
+                valores = (persona.id_persona,)
+                cursor.execute(cls._ELIMINAR, valores)
+                log.debug(f'Los objetos eliminados  son: {persona}')
+                return cursor.rowcount
+
+
+if __name__ == '__main__':
+    # Eliminar un registro
+    # persona1 = Persona(id_persona=8)
+    # personas_eliminadas = PersonaDAO.eliminar(persona1)
+    # log.debug(f'Personas eliminadas: {personas_eliminadas}')
+
+    # Seleccionar objetos
+    personas = PersonaDAO.seleccionar()
+    for persona in personas:
+        log.debug(persona)
