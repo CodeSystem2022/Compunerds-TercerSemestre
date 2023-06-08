9c4c21afc3fd66181d39b1535fc9ec647fea0899
Giuliana Dealbera Etchechoury <giulydealbera@gmail.com>
2023-06-07 22:18:00 -0300

    Giu sube clase 8 de java (completo)

--- Java/Leccion8/ManejoExcepciones1/src/excepciones/OperacionExcepcion.java
+++ Java/Leccion8/ManejoExcepciones1/src/excepciones/OperacionExcepcion.java
@@ -4,8 +4,14 @@
  */
 package excepciones;
 
+//public class OperacionExcepcion extends Exception {
+//    //Constructor
+//    public OperacionExcepcion(String mensaje){
+//        super(mensaje);
+//    }
+//}
 
-public class OperacionExcepcion extends Exception {
+public class OperacionExcepcion extends RuntimeException { // la diferencia radica en el compilador. No obliga a procesar este tipo de excep.
     //Constructor
     public OperacionExcepcion(String mensaje){
         super(mensaje);
--- Java/Leccion8/ManejoExcepciones1/src/test/TestExcepciones.java
+++ Java/Leccion8/ManejoExcepciones1/src/test/TestExcepciones.java
@@ -4,6 +4,9 @@
  */
 package test;
 
+import static aritmetica.Aritmetica.division;
+import excepciones.OperacionExcepcion;
+
 
 public class TestExcepciones {
     public static void main(String[] args) {
@@ -11,11 +14,19 @@ public class TestExcepciones {
         //System.out.println("resultado = " + resultado);
         
         int resultado = 0;
-        try{
-            resultado = 10/0;
-        } catch(Exception e){
+        try{ 
+//          resultado = 10/0;
+            resultado = division(10,0);
+        } catch(OperacionExcepcion e){
+            System.out.println("Ocurrió un error de tipo OperacionExcepcion"); //repotando la excepcion
+            System.out.println(e.getMessage());
+        } catch(Exception e){  // Se puede hacer varios catch RESPETANDO las jerarquias. La mayor va abajo.
             System.out.println("Ocurrió un ERROR");
             e.printStackTrace(System.out); //se conoce como pila de excepciones
+            System.out.println(e.getMessage());
+        } 
+        finally{
+            System.out.println("Se revisó la división entre cero"); // SIEMPRE se ejecuta
         }
         System.out.println("La variable de resultado tiene como valor: " + resultado);
     }
