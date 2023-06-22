a67181cb9d0c2562f73e9fdbfff8617c2b020139
carlafaes <carlafaes18@hotmail.com>
2023-06-22 02:17:19 -0300

    creacion de metodo conexion

--- Python/laboratorioIII-python/Clase-7/capa_datos_persona/Persona.py
+++ Python/laboratorioIII-python/Clase-7/capa_datos_persona/Persona.py
@@ -1,5 +1,7 @@
+from logger_base import log 
+
 class Persona:
-    def __init__(self, id_persona, nombre, apellido, email):
+    def __init__(self, id_persona=None, nombre=None, apellido=None, email=None):
         self.id_persona = id_persona
         self.nombre = nombre
         self.apellido = apellido
@@ -54,3 +56,11 @@ def email(self):
 @email.setter
 def email(self, email):
     self.email = email
+
+if __name__ == '__main__':
+    persona1= Persona(1,'Juan', 'Perez','jperez@gmail.com')
+    log.debug(persona1)
+    persona2= Persona(nombre='Jose',apellido='Lepez',email='jlepez@gmail.com')
+    log.debug(persona2)
+    persona1=Persona(id_persona=1)
+    log.debug(persona1)
