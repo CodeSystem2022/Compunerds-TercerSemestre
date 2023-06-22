d1692d1f4c6e39b19500171af3d7f742130638bc
carlafaes <carlafaes18@hotmail.com>
2023-06-22 00:39:33 -0300

    CONEXION A BASE DE DATOS

--- Python/laboratorioIII-python/Clase-4/BD/prueba_bd.py
+++ Python/laboratorioIII-python/Clase-4/BD/prueba_bd.py
@@ -1,11 +1,11 @@
-import psycopg2 # esto es para poder conectarnos a Postgre
+import psycopg2 # esto es para poder conectarnos a Postgres database
 
 conexion = psycopg2.connect(
-    user='postgresSQL',
-    password='admin',
-    host='localhost',
-    port='27017',
-    database='test_db'
+    user='carla', ##SE DEBE MODIFICAR SEGUN EL NOMBRE DE USUARIO DE SU PROPIA CUENTA DE POSTGRES
+    password='12345',##SE DEBE MODIFICAR SEGUN LA CONTRASEÑA DE SU CUENTA DE POSTGRES
+    host='localhost',##SE DEBE MODIFICAR SEGUN EL NOMBRE DE SU HOST LOCAL
+    port='5432',##SE DEBE MODIRFICAR SEGUN SU NUMERO DE PUERTO
+    database='test_bd'
 )
 
 cursor = conexion.cursor()
